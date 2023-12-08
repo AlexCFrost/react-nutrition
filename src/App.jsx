41ccc0bb30cfdc5c4ae83da07233e303fb3f0f25
AlexCFrost <siva.ganesh.c18@gmail.com>
2023-12-08 14:51:26 +0530

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,50 @@
-import React,{ Component } from 'react'
-import './App.css'
-import FoodBox from './Components/FoodBox';
-import FoodData from './Components/FoodData'
-import Search from './components/Search';
+import { Component } from "react";
+import "./App.css";
+import FoodBox from "./Components/FoodBox";
+import FoodData from "./Components/FoodData";
+import Search from "./components/Search";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      search:"",
-      count:0
-    }
+      search: "",
+      count: 0,
+    };
   }
 
   handleSearch = (info) => {
-    this.setState({search:info})
-  }
+    this.setState({ search: info });
+  };
 
   render() {
-
     return (
       <>
-      <div>
-        <Search handleSearch={this.handleSearch} />
-        {FoodData.filter((el)=>{
-
-            if(this.state.search==""){
-              return el
+        <div>
+          <Search handleSearch={this.handleSearch} />
+          {FoodData.filter((el) => {
+            if (this.state.search == "") {
+              return el;
+            } else if (
+              el.name.toLowerCase().includes(this.state.search.toLowerCase())
+            ) {
+              return el;
             }
-            else if(el.name.toLowerCase().includes(this.state.search.toLowerCase())){
-              return el
-            }
-          }).map((el)=>{
-         return <div key={el.id}>
-             <FoodBox food={el} count={this.state.count} handleSearch={this.handleSearch}/>
-          </div>
-        })}
-      </div>
+          }).map((el) => {
+            return (
+              <div key={el.id}>
+                <FoodBox
+                  food={el}
+                  count={this.state.count}
+                  handleSearch={this.handleSearch}
+                />
+              </div>
+            );
+          })}
+        </div>
       </>
-    )
+    );
   }
 }
 
-
-
-export default App
-
-
-
+export default App;
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,16 +1,19 @@
-import  { Component } from 'react'
+/* eslint-disable react/prop-types */
+import { Component } from "react";
 
 export default class Search extends Component {
   render() {
     return (
-      <div  id="header">
-         <div id="search">
+      <div id="header">
+        <div id="search">
           <h3>Search</h3>
-        <input type="text" placeholder='Enter Something' 
-        onChange={(e) => this.props.handleSearch(e.target.value)}/>
+          <input
+            type="text"
+            placeholder="Enter Something"
+            onChange={(e) => this.props.handleSearch(e.target.value)}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
-
